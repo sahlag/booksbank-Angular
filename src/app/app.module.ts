0f6ff529cd758ba414b91556cb0f5b45e64a916d
souheil <boussaid-souheil@live.fr>
2018-12-06 15:27:31 +0100

    App Structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SighupComponent } from './auth/signup/sighup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthService } from './service/auth.service';
+import { BooksService } from './service/books.service';
+import { AuthGuardService } from './service/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SighupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/view/:id', component: SingleBookComponent }
+];
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SighupComponent,
+    SigninComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
+  ],
+  providers: [
+    AuthService,
+    BooksService,
+    AuthGuardService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/auth/signup/sighup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sighup',
+  templateUrl: './sighup.component.html',
+  styleUrls: ['./sighup.component.scss']
+})
+export class SighupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
