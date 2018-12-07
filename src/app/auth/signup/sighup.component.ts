fdda06eb1eda7da532389cdcc608e0f2197d73f0
souheil <boussaid-souheil@live.fr>
2018-12-07 18:49:52 +0100

    Authontification/signup

--- src/app/auth/signup/sighup.component.ts
+++ src/app/auth/signup/sighup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sighup',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SighupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+// initiation du formulaire
+
+  initForm() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+// envoi du formulaire
+
+  onSubmit() {
+    const email = this.signupForm.get('email').value;
+    const password = this.signupForm.get('password').value;
+
+    this.authService.createNewUser(email, password).then(
+      () => {
+        this.router.navigate(['/books']);
+      },
+      (error) => {
+          this.errorMessage = error;
+      }
+    );
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+
+
+@Injectable()
 export class AuthService {
 
   constructor() { }
+
+// creation d'un nouvel utilisateur
+
+  createNewUser(email: string, password: string) {
+    return new Promise (
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Connextion d'un utilisateur existant
+
+  signInUser(email: string, password: string) {
+    return new Promise (
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+            (error) => {
+             reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Déconnextion
+
+  signOutUser() {
+    firebase.auth().signOut();
+  }
 }
